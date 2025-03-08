7fe8b0d0c47e0dfb72bda8da7dcc4434e1a230ea
Saurav Kumar <165908739+saurav2520@users.noreply.github.com>
2025-03-08 16:55:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 async function getWeather() {
-    const apiKey = 'f8e8011454d2b7f9ed71de9a67151be2'; 
+    // const apiKey = 'f8e8011454d2b7f9ed71de9a67151be2'; 
     const city = document.getElementById('city').value;
     if (!city) return;
     
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     try {
         const response = await fetch(url);
         const data = await response.json();
